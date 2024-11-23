ad46e1b55f83a95df96d198751de646926e266f7
Tarık KAMAT <tarikkamat2@gmail.com>
2024-11-23 04:10:39 +0300

    override post method in GetAllUsers to return a bad request response for disabled feature

--- backend/project/users/api/views.py
+++ backend/project/users/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework_simplejwt.views import TokenObtainPairView
 
+from project.core.responses import APIResponse
 from project.core.views import BaseListView, BaseDetailView
 from users.api.serializers import UserSerializer, MyTokenObtainPairSerializer
 from users.models import User
@@ -8,6 +9,10 @@ from users.models import User
 class GetAllUsers(BaseListView):
     serializer_class = UserSerializer
     model = User
+
+    def post(self, request):
+        # Override edilen post methodu silinirse BaseModel ile bu özellik aktif edilebilir.
+        return APIResponse.bad_request({"message:": "This feature is disabled.", "status": False})
 
 
 class GetUser(BaseDetailView):
