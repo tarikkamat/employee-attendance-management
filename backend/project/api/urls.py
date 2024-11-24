16638f472488727c669e99a48b8ee98ac18e530c
Tarık KAMAT <tarikkamat2@gmail.com>
2024-11-24 22:46:10 +0300

    add authentication endpoints for token validation and logout

--- backend/project/api/urls.py
+++ backend/project/api/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
 
+from leaves.api import views as leave_views
+from leaves.models import Leave
 from project.helpers import get_api_slug
 from users.api import views as user_views
 from users.models import User
-from leaves.models import Leave
-from leaves.api import views as leave_views
 
 urlpatterns = [
     path(get_api_slug(User) + "/", user_views.GetAllUsers.as_view()),
     path(get_api_slug(User) + "/<int:pk>", user_views.GetUser.as_view()),
     path(get_api_slug(Leave) + "/", leave_views.GetAllLeaves.as_view()),
     path(get_api_slug(Leave) + "/<int:pk>", leave_views.GetLeave.as_view()),
-    path('auth/login', user_views.MyTokenObtainPairView.as_view())
+    path('auth/login', user_views.MyTokenObtainPairView.as_view()),
+    path('auth/validate', user_views.MyTokenValidateView.as_view()),
+    path('auth/logout', user_views.MyTokenRemoveView.as_view())
 ]
